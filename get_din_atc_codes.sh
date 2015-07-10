b1d4629eef500e2342ebf14438701ce2b7af8d55
Raymond Rusk <rusk.raymond@gmail.com>
2015-07-10 12:26:58 -0700

    Restructure directories

--- get_din_atc_codes.sh
+++ get_din_atc_codes.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
-if [ ! -d update ]; then
-  mkdir update
-  touch update/.gitkeep
+if [ ! -d download ]; then
+  mkdir download
 fi
-cd update
+cd download
+rm drug*.zip* ther*.zip*
 wget http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/ther.zip || curl -O http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/ther.zip
 wget http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/ther_ia.zip || curl -O http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/ther_ia.zip
 wget http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/drug.zip || curl -O http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/drug.zip
 wget http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/drug_ia.zip || curl -O http://www.hc-sc.gc.ca/dhp-mps/alt_formats/zip/prodpharma/databasdon/drug_ia.zip
-unzip ther.zip
-unzip ther_ia.zip
-unzip drug.zip
-unzip drug_ia.zip
+if [ ! -d ../data ]; then
+  mkdir ../data
+fi
+cd ../data
+unzip -o ../download/ther.zip
+unzip -o ../download/ther_ia.zip
+unzip -o ../download/drug.zip
+unzip -o ../download/drug_ia.zip
